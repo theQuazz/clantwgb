4f7b74dd6acdf66038d0a1fb4495212e3756dc62
theQuazz <djrquazz@gmail.com>
2013-04-05 18:04:44 -0400

    Starting Clan TwGB Website in Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clantwgb::Application.config.secret_token = '6022b04ea932bcb4d11f7755a041e5c3d4bc1e89965370d7981994a75520ccd6a63bacaf5d7d4c298b23e7061be2b1ee72366a6e1fba2bc5416879494950e6c1'
